b9370d7f3b40dcbe792213f3aba3086d4137f4f1
wsdesign <info.wsdesignuiux@gmail.com>
2018-12-05 14:58:41 +0530

    Initial commit

---
+++ app/src/main/java/e/wolfsoft1/contactlist/Collapsing_Contacts.java
@@ -0,0 +1,24 @@
+package e.wolfsoft1.contactlist;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class Collapsing_Contacts extends AppCompatActivity {
+
+    TextView pricetexttoolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing__contacts);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Oscar Armer");
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+//
+
+    }
+}
